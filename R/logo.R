f2022cd8f081700a12565836f19020f2c1a5654f
James Clawson <clawson@gmail.com>
2024-01-17 17:23:23 -0600

    modification to hexsticker

--- R/logo.R
+++ R/logo.R
@@ -1,24 +1,26 @@
-# img <- png::readPNG("~/Downloads/bannr2.png")
+# img <- png::readPNG("~/Downloads/bannr3.png")
 # g <- grid::rasterGrob(img, interpolate=TRUE)
 # 
 # p <- ggplot2::qplot(1:10, 1:10, geom="blank") +
 #   ggplot2::annotation_custom(
-#     g, 
-#     xmin=-Inf, xmax=Inf, 
-#     ymin=-Inf, ymax=Inf) + 
+#     g,
+#     xmin=-Inf, xmax=Inf,
+#     ymin=-Inf, ymax=Inf) +
 #   ggplot2::theme_void() +
 #   hexSticker::theme_transparent()
 # 
 # hexSticker::sticker(
-#   p, 
+#   p,
 #   s_width = .7,
 #   s_height = .7,
 #   s_x = 1,
 #   s_y = 0.69,
-#   package = "bannr", 
+#   package = "bannr",
 #   p_color = "black",
 #   p_y = 1.35,
 #   p_size = 38,
 #   h_fill = "white",
 #   h_color = "black",
 #   filename = "man/figures/logo.png")
+# 
+# usethis::use_logo("man/figures/logo.png")
